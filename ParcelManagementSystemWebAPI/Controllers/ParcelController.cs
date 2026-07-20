53b0e9f4232de04f5303aa0407b19d72f595677f
Swathi Appari <8938020@ups.com>
2026-07-20 18:08:09 +0530

    Sprint 1 Day 2: Added Parcel model, GET APIs, routing and Swagger testing

---
+++ ParcelManagementSystemWebAPI/Controllers/ParcelController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParcelManagementSystemWebAPI.Model;
+
+namespace ParcelManagementSystemWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ParcelController : ControllerBase
+    {
+        private static readonly List<Parcel> Parcels = new List<Parcel>
+        {
+            new Parcel
+            {
+                Id = 1,
+                TrackingNumber = "TRK1001",
+                SenderName = "Swathi",
+                ReceiverName = "Rahul",
+                Status = "Booked"
+            },
+            new Parcel
+            {
+                Id = 2,
+                TrackingNumber = "TRK1002",
+                SenderName = "John",
+                ReceiverName = "David",
+                Status = "In Transit"
+            }
+        };
+
+        [HttpGet] //"This method should execute only when a GET request is received."
+        public IActionResult GetParcels() //"This method will return an HTTP response."
+        {
+           
+            return Ok(Parcels); //Ok() is a helper method provided by ControllerBase.
+        }
+
+        [HttpGet("{id}")] //"This method should execute only when a GET request is received with an id parameter."
+        public IActionResult GetParcelById(int id)
+        {
+            
+            var parcel = Parcels.FirstOrDefault(p => p.Id == id);
+            if (parcel == null)
+            {
+                return NotFound(); //NotFound() is a helper method provided by ControllerBase.
+            }
+
+            return Ok(parcel);
+        }
+    }
+}
---
+++ ParcelManagementSystemWebAPI/Model/Parcel.cs
@@ -0,0 +1,11 @@
+﻿namespace ParcelManagementSystemWebAPI.Model
+{
+    public class Parcel
+    {
+        public int Id { get; set; }
+        public string TrackingNumber { get; set; }
+        public string SenderName { get; set; }
+        public string ReceiverName { get; set; }
+        public string Status { get; set; }
+    }
+}
